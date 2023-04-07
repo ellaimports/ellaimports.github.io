9b636c36ffbcca40aefaa2bb5c439757285ad1b7
ellaimports <130144057+ellaimports@users.noreply.github.com>
2023-04-07 15:13:40 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>Ella Imports - Buy Furniture Vancouver</title>
+</head>
+<body>
+<center>
+<ul>
+<li>Buy furniture Vancouver</li>
+<li>furniture stores vancouver</li>
+<li>Vancouver furniture stores</li>
+<li>Furniture stores Richmond</li>
+<li>Furniture stores Coquitlam</li>
+<li>Sofa bed Vancouver</li>
+<li>Online sofa stores</li>
+<li>Vancouver furniture</li>
+<li>buy bed vancouver</li>
+<li>Couch for sale near me</li>
+<li>Couch stores</li>
+<li>Liquidation furniture</li>
+<li>Furniture shop</li>
+<li>Furniture shops</li>
+<li>Furniture store Surrey</li>
+<li>Furniture stores Langley</li>
+</ul>
+</center>
+</body>
+</html>
